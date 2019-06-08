336e42fcf0c57efa32c7420da72e4f533aaefb23
eE850702 <khtp91113@gmail.com>
2019-06-08 23:58:13 +0800

    finish record, adjust volume, mix vocal & music

--- app/src/main/java/com/example/karaoke/MusicActivity.java
+++ app/src/main/java/com/example/karaoke/MusicActivity.java
@@ -3,24 +3,38 @@ package com.example.karaoke;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaCodec;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.media.PlaybackParams;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
@@ -29,9 +43,18 @@ import java.util.List;
 public class MusicActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer = null;
+    private MediaPlayer mediaPlayerLeft = null;
+    private MediaPlayer mediaPlayerRight = null;
     private float pitch;
     private int pitchState;
     private final float pitch_interval = 1.05946f;
+    private int song_id = R.raw.onion_mayday_cut;
+    private Handler handler;
+    private final int SHOW_LISTEN_BUTTON = 0;
+    private float musicVolume = 0.5f;
+    private float vocalVolume = 0.5f;
+    private String outputRecordPath;
+    private String outputMusicPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +67,87 @@ public class MusicActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
             }
         }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
+            }
+        }
 
+        // set seekbar listener
+        SeekBar seekbarMusic = findViewById(R.id.seekBar_music);
+        SeekBar seekbarVocal = findViewById(R.id.seekBar_vocal);
+        seekbarMusic.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView musicValue = findViewById(R.id.music_value);
+                musicValue.setText(String.valueOf(progress) + "%");
+                musicVolume = progress / 100.0f;
+                if (mediaPlayerLeft != null){
+                    mediaPlayerLeft.setVolume(musicVolume, 0f);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        seekbarVocal.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView vocalValue = findViewById(R.id.vocal_value);
+                vocalValue.setText(String.valueOf(progress) + "%");
+                vocalVolume = progress / 100.0f;
+                if (mediaPlayerRight != null){
+                    mediaPlayerRight.setVolume(0f, vocalVolume);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        // hide record related buttons
+        findViewById(R.id.listen).setVisibility(View.INVISIBLE);
+        findViewById(R.id.text_music).setVisibility(View.INVISIBLE);
+        findViewById(R.id.text_vocal).setVisibility(View.INVISIBLE);
+        findViewById(R.id.seekBar_music).setVisibility(View.INVISIBLE);
+        findViewById(R.id.seekBar_vocal).setVisibility(View.INVISIBLE);
+        findViewById(R.id.music_value).setVisibility(View.INVISIBLE);
+        findViewById(R.id.vocal_value).setVisibility(View.INVISIBLE);
+        findViewById(R.id.adjust).setVisibility(View.INVISIBLE);
+        findViewById(R.id.save).setVisibility(View.INVISIBLE);
+        findViewById(R.id.share).setVisibility(View.INVISIBLE);
+        findViewById(R.id.upload).setVisibility(View.INVISIBLE);
+
+        handler = new Handler(){
+            @Override
+            public void handleMessage(Message message){
+                // show listen button, hide key increase & decrease button
+                if (message.what == SHOW_LISTEN_BUTTON){
+                    findViewById(R.id.listen).setVisibility(View.VISIBLE);
+                    findViewById(R.id.adjust).setVisibility(View.VISIBLE);
+                    findViewById(R.id.save).setVisibility(View.VISIBLE);
+                    findViewById(R.id.share).setVisibility(View.VISIBLE);
+                    findViewById(R.id.upload).setVisibility(View.VISIBLE);
+                    findViewById(R.id.key_decrease).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.key_increase).setVisibility(View.INVISIBLE);
+                }
+            }
+        };
+
+        outputMusicPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/music.wav";
+        outputRecordPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/record.wav";
     }
 
     public void loadMusic(View view) throws IOException {
@@ -53,19 +156,32 @@ public class MusicActivity extends AppCompatActivity {
             return;
         }
         // decode mp3 file, remove vocal, save as wav file
-        String outputPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/test.wav";
-        decode(outputPath); // it takes about 30 seconds
-
+        File file = new File(outputMusicPath);
+        // check if file exist
+        if(file.exists() == false)
+            decode(outputMusicPath); // it takes about 30 seconds
         // open music file and play
-        File file = new File(outputPath);
         Uri songUri = Uri.fromFile(file);
         mediaPlayer = MediaPlayer.create(getApplicationContext(), songUri);
         pitch = 1;
         pitchState = 0; // increase or decrease up to 5 levels
-        mediaPlayer.start();
+
+        // use thread to record
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                String outputRecordPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/record.wav";
+                try {
+                    record(outputRecordPath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     public void keyIncrease(View view){
+        Log.i("test", "increase press");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             PlaybackParams params = new PlaybackParams();
             if (pitchState == 5){
@@ -80,9 +196,10 @@ public class MusicActivity extends AppCompatActivity {
     }
 
     public void keyDecrease(View view){
+        Log.i("test", "decrease press");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             PlaybackParams params = new PlaybackParams();
-            if (pitch == -5){
+            if (pitchState == -5){
                 Toast.makeText(this, "Lowest", Toast.LENGTH_SHORT).show();
                 return;
             }
@@ -95,7 +212,7 @@ public class MusicActivity extends AppCompatActivity {
 
     // decode mp3 to PCM, then convert PCM to wav
     private void decode(String outputPath) throws IOException {
-        final AssetFileDescriptor fd = getResources().openRawResourceFd(R.raw.onion_mayday);
+        final AssetFileDescriptor fd = getResources().openRawResourceFd(song_id);
         MediaExtractor extractor = null;
         MediaFormat mediaFormat = null;
 
@@ -191,45 +308,8 @@ public class MusicActivity extends AppCompatActivity {
                 }
             }
 
-            //write wav header
-            short channel = 1;
-            int sampleRate = 44100;
-            short bitDepth = 16;
-
-            byte[] chunkSize = ByteBuffer.allocate(4)
-                                .order(ByteOrder.LITTLE_ENDIAN)
-                                .putInt(36 + outputList.size() * channel * bitDepth / 8)
-                                .array();
-
-            byte[] headers = ByteBuffer.allocate(14)
-                    .order(ByteOrder.LITTLE_ENDIAN)
-                    .putShort(channel)
-                    .putInt(sampleRate)
-                    .putInt(sampleRate * channel * bitDepth / 8)
-                    .putShort((short) (channel * bitDepth / 8))
-                    .putShort(bitDepth)
-                    .array();
-
-            byte[] subChunk2Size = ByteBuffer.allocate(4)
-                                    .order(ByteOrder.LITTLE_ENDIAN)
-                                    .putInt(outputList.size() * channel * bitDepth / 8)
-                                    .array();
-
-            output.write(new byte[]{
-                            'R', 'I', 'F', 'F' //chunk ID
-                    });
-            output.write(chunkSize);
-            output.write(new byte[]{
-                    'W', 'A', 'V', 'E', //format
-                    'f', 'm', 't', ' ', //subchunk1 ID
-                    16,   0,   0,   0, //subchunk1 size
-                     1,   0 //audio format
-            });
-            output.write(headers);
-            output.write(new byte[]{
-                    'd', 'a', 't', 'a' //subchunk2 ID
-            });
-            output.write(subChunk2Size);
+            // write wav header
+            writeWavHeader(output, outputList);
 
             // convert Byte List to byte array
             Byte[] soundBytes = outputList.toArray(new Byte[outputList.size()]);
@@ -247,4 +327,213 @@ public class MusicActivity extends AppCompatActivity {
         }
     }
 
+    private void writeWavHeader(FileOutputStream output, List<Byte> outputList) throws IOException {
+        //write wav header
+        short channel = 1;
+        int sampleRate = 44100;
+        short bitDepth = 16;
+
+        byte[] chunkSize = ByteBuffer.allocate(4)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .putInt(36 + outputList.size() / 2 * channel * bitDepth / 8)
+                .array();
+
+        byte[] headers = ByteBuffer.allocate(14)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .putShort(channel)
+                .putInt(sampleRate)
+                .putInt(sampleRate * channel * bitDepth / 8)
+                .putShort((short) (channel * bitDepth / 8))
+                .putShort(bitDepth)
+                .array();
+
+        byte[] subChunk2Size = ByteBuffer.allocate(4)
+                .order(ByteOrder.LITTLE_ENDIAN)
+                .putInt(outputList.size() / 2 * channel * bitDepth / 8)
+                .array();
+
+        output.write(new byte[]{
+                'R', 'I', 'F', 'F' //chunk ID
+        });
+        output.write(chunkSize);
+        output.write(new byte[]{
+                'W', 'A', 'V', 'E', //format
+                'f', 'm', 't', ' ', //subchunk1 ID
+                16,   0,   0,   0, //subchunk1 size
+                1,   0 //audio format
+        });
+        output.write(headers);
+        output.write(new byte[]{
+                'd', 'a', 't', 'a' //subchunk2 ID
+        });
+        output.write(subChunk2Size);
+    }
+
+    private void record(String outputPath) throws IOException {
+        int minSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100,  AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, minSize);
+
+        byte[] data = new byte[minSize];
+        List<Byte> outputList = new ArrayList<Byte>();
+
+        FileOutputStream output = new FileOutputStream(outputPath);
+
+        audioRecord.startRecording();
+        mediaPlayer.start();
+
+        while(mediaPlayer.isPlaying()){
+            int size = audioRecord.read(data, 0, minSize);
+            if (size != AudioRecord.ERROR_INVALID_OPERATION){
+                for (byte b: data)
+                    outputList.add(b);
+            }
+        }
+        audioRecord.stop();
+        audioRecord.release();
+
+        writeWavHeader(output, outputList);
+        Byte[] soundBytes = outputList.toArray(new Byte[outputList.size()]);
+        byte[] outputSound = new byte[soundBytes.length];
+        int j = 0;
+        for(Byte b: soundBytes)
+            outputSound[j++] = b.byteValue();
+        // write wav data
+        output.write(outputSound);
+
+        output.close();
+
+        // show listen button
+        Message msg = Message.obtain();
+        msg.what = SHOW_LISTEN_BUTTON;
+        handler.sendMessage(msg);
+    }
+
+    // listen to record mix with song
+    public void listen_record(View view){
+        if (mediaPlayerLeft == null && mediaPlayerRight == null){
+            // open music & record file and play
+            File fileMusic = new File(outputMusicPath);
+            //File fileRecord = new File(outputRecordPath);
+            Uri musicUri = Uri.fromFile(fileMusic);
+            //Uri recordUri = Uri.fromFile(fileRecord);
+            mediaPlayerLeft = MediaPlayer.create(getApplicationContext(), musicUri);
+            //mediaPlayerRight = MediaPlayer.create(getApplicationContext(), recordUri);
+
+            /////// for test!!!!!! ////////////
+            mediaPlayerRight = MediaPlayer.create(getApplicationContext(), R.raw.onion_mayday_cut_wav);
+
+            // play music in left channel, vocal in right channel
+            // get volume, default 50% vocal 50% music
+            mediaPlayerLeft.setVolume(musicVolume, 0f);
+            mediaPlayerRight.setVolume(0f, vocalVolume);
+
+            mediaPlayerLeft.start();
+            mediaPlayerRight.start();
+        }
+        else if(mediaPlayerLeft.isPlaying() == false && mediaPlayerRight.isPlaying() == false){
+            // play music in left channel, vocal in right channel
+            // get volume, default 50% vocal 50% music
+            mediaPlayerLeft.setVolume(musicVolume, 0f);
+            mediaPlayerRight.setVolume(0f, vocalVolume);
+
+            mediaPlayerLeft.start();
+            mediaPlayerRight.start();
+        }
+    }
+
+    public void show_adjust(View view){
+        int state = findViewById(R.id.text_music).getVisibility();
+        // hide seekbar & text
+        if (state == View.VISIBLE){
+            findViewById(R.id.text_vocal).setVisibility(View.INVISIBLE);
+            findViewById(R.id.text_music).setVisibility(View.INVISIBLE);
+            findViewById(R.id.seekBar_vocal).setVisibility(View.INVISIBLE);
+            findViewById(R.id.seekBar_music).setVisibility(View.INVISIBLE);
+            findViewById(R.id.music_value).setVisibility(View.INVISIBLE);
+            findViewById(R.id.vocal_value).setVisibility(View.INVISIBLE);
+        }
+        // show seekbar & text
+        else{
+            findViewById(R.id.text_vocal).setVisibility(View.VISIBLE);
+            findViewById(R.id.text_music).setVisibility(View.VISIBLE);
+            findViewById(R.id.seekBar_vocal).setVisibility(View.VISIBLE);
+            findViewById(R.id.seekBar_music).setVisibility(View.VISIBLE);
+            findViewById(R.id.music_value).setVisibility(View.VISIBLE);
+            findViewById(R.id.vocal_value).setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void save(View view) throws IOException {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            // stop mediaplayer
+            if (mediaPlayerRight != null && mediaPlayerRight.isPlaying()) {
+                mediaPlayerRight.stop();
+                mediaPlayerRight = null;
+            }
+            if (mediaPlayerLeft != null && mediaPlayerLeft.isPlaying()){
+                mediaPlayerLeft.stop();
+                mediaPlayerLeft = null;
+            }
+            // read file
+            InputStream musicStream = new FileInputStream(outputMusicPath);
+            //InputStream vocalStream = new FileInputStream(outputRecordPath);
+
+            ////// For test //////
+            InputStream vocalStream = getResources().openRawResource(R.raw.onion_mayday_cut_wav);
+
+            byte[] music = new byte[musicStream.available()];
+            musicStream.read(music);
+            byte[] vocal = new byte[vocalStream.available()];
+            vocalStream.read(vocal);
+
+            List<Byte> outputList = new ArrayList<Byte>();
+            // mix vocal & music, ignore 44 bytes header
+            for (int i = 44; i < music.length; i += 2){
+                // convert byte to short (16 bits)
+                float sampleMusic = (music[i] & 0xFF | music[i+1] << 8) / 32768.0f;
+                float sampleVocal = (vocal[i] & 0xFF | vocal[i+1] << 8) / 32768.0f;
+
+                // adjust volume & mix
+                sampleMusic *= musicVolume;
+                sampleVocal *= vocalVolume;
+                float mix = (sampleMusic + sampleVocal);
+                // avoid too many noise
+                if (mix > 1.0f)
+                    mix = 1.0f;
+                else if (mix < -1.0f)
+                    mix = -1.0f;
+                // save byte
+                short output = (short)(mix * 32768.0f);
+                outputList.add((byte)(output & 0xFF));
+                outputList.add((byte)(output >> 8));
+            }
+            musicStream.close();
+            vocalStream.close();
+
+            // save
+            FileOutputStream output = new FileOutputStream(outputRecordPath);
+            // write wav header
+            writeWavHeader(output, outputList);
+
+            // convert Byte List to byte array
+            Byte[] soundBytes = outputList.toArray(new Byte[outputList.size()]);
+            byte[] outputSound = new byte[soundBytes.length];
+            int j = 0;
+            for(Byte b: soundBytes)
+                outputSound[j++] = b.byteValue();
+            // write wav data
+            output.write(outputSound);
+
+            output.close();
+            Toast.makeText(this, "File Saved", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void share(View view){
+        // TODO
+    }
+
+    public void upload(View view){
+        // TODO
+    }
 }
