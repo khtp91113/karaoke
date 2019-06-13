32258007227beaa0c211040fc6ac753e1b613841
yuting0203 <koko0108165@gmail.com>
2019-06-13 17:27:30 +0800

    add LyricView, LyricRow, LyricBuilder

---
+++ app/src/main/java/com/example/karaoke/LyricBuilder.java
@@ -0,0 +1,97 @@
+package com.example.karaoke;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 解析歌詞
+ */
+
+public class LyricBuilder {
+    static final String TAG = "LyricBuilder";
+
+    public List<LyricRow> getLrcRows(String rawLrc){
+        if (rawLrc == null || rawLrc.length() == 0) return null;
+
+
+        StringReader reader = new StringReader(rawLrc);
+        BufferedReader br = new BufferedReader(reader);
+        String line = null;
+        List<LyricRow> rows = new ArrayList<LyricRow>();
+        try {
+            do {
+                line = br.readLine();
+
+                if (line != null && line.length() > 0) {
+
+                    List<LyricRow> lrcRows = new ArrayList<LyricRow>();
+
+                    try {
+
+                        //從"]"，拆成前後兩段
+                        String content = line.substring(10);
+                        String times = line.substring(0, 10).replace("[", "").replace("]", "");
+
+                        LyricRow lrcRow = new LyricRow();
+                        lrcRow.setContent(content);
+                        long startTime = timeConvert(times);
+                        lrcRow.setStartTime(startTime);
+                        lrcRows.add(lrcRow);
+
+
+                    } catch (Exception e) {
+                                            }
+
+                    if (lrcRows != null && lrcRows.size() > 0) {
+                        for (LyricRow row : lrcRows) {
+                            rows.add(row);
+                        }
+                    }
+                }
+            } while (line != null);
+
+            if (rows.size() > 0) {
+
+                if (rows != null && rows.size() > 0) {
+                    int size = rows.size();
+                    for (int i = 0; i < size; i++) {
+                        LyricRow lrcRow = rows.get(i);
+
+                  if (i < size - 1) {
+                            lrcRow.setEndTime(rows.get(i + 1).getStartTime());
+                        } else {
+                            lrcRow.setEndTime(lrcRow.getStartTime() + 10000);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            return null;
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            reader.close();
+        }
+        return rows;
+    }
+
+
+    /**
+     * 將時間由String轉成Long Type
+     */
+    private static long timeConvert(String timeString) {
+
+        timeString = timeString.replace('.', ':');
+        String[] times = timeString.split(":");
+
+        return Integer.valueOf(times[0]) * 60 * 1000 + Integer.valueOf(times[1]) * 1000 + Integer.valueOf(times[2]);
+    }
+}
---
+++ app/src/main/java/com/example/karaoke/LyricView.java
@@ -0,0 +1,160 @@
+package com.example.karaoke;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.List;
+
+public class LyricView extends View {
+    public final static String TAG = "LyricView";
+
+
+
+    /**
+     * 所有歌詞
+     */
+    private List<LyricRow> mLrcRows;
+
+    /**Highlight歌詞行數     */
+    private int mHighLightRow = 0;
+    /**Highlight歌詞顏色     */
+    private int mHighLightRowColor = Color.YELLOW;
+    /**其他歌詞顏色     */
+    private int mNormalRowColor = Color.WHITE;
+
+
+    /**歌詞字體大小預設值*/
+    private int mLrcFontSize = 50;
+
+    /**Highlight歌詞字體大小預設值*/
+    private int mHighLightFontSize = 70;
+
+    /**歌詞間距     **/
+    private int mPaddingY = 20;
+
+
+    private Paint mPaint;
+
+    /**
+     *  當前時間
+     */
+    long currentMillis;
+
+
+    public LyricView(Context context, AttributeSet attr) {
+        super(context, attr);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setTextSize(mLrcFontSize);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        final int height = getHeight();
+
+        int rowY = 0;
+        final int rowX = 20; //離左邊的距離
+        int rowNum = 0;
+
+
+        //Highlight歌詞
+        int highlightRowY = height / 2 - mLrcFontSize;
+        drawKaraokeHighLightLrcRow(canvas, rowX, highlightRowY);
+
+
+        mPaint.setColor(mNormalRowColor);
+        mPaint.setTextSize(mLrcFontSize);
+        mPaint.setTextAlign(Paint.Align.LEFT);
+
+        //其餘歌詞
+        rowNum = mHighLightRow - 1;
+        rowY = highlightRowY - mPaddingY - mLrcFontSize;
+        //顯示前一句歌詞
+        if (rowY > -mLrcFontSize && rowNum >= 0) {
+            String text = mLrcRows.get(rowNum).content;
+            canvas.drawText(text, rowX, rowY, mPaint);
+        }
+
+
+        rowNum = mHighLightRow + 1;
+        rowY = highlightRowY + mPaddingY + mLrcFontSize;
+
+
+        //顯示後面的歌詞
+        while (rowY < height && rowNum < mLrcRows.size()) {
+            String text = mLrcRows.get(rowNum).content;
+            canvas.drawText(text, rowX, rowY, mPaint);
+            rowY += (mPaddingY + mLrcFontSize);
+            rowNum++;
+        }
+
+    }
+
+    private void drawKaraokeHighLightLrcRow(Canvas canvas, int rowX, int highlightRowY) {
+        LyricRow highLrcRow = mLrcRows.get(mHighLightRow);
+        String highlightText = highLrcRow.content;
+
+        // 普通顏色
+        mPaint.setColor(mNormalRowColor);
+        mPaint.setTextSize(mHighLightFontSize);
+        mPaint.setTextAlign(Paint.Align.LEFT);
+        canvas.drawText(highlightText, rowX, highlightRowY, mPaint);
+
+        // 逐字highlight
+        int highLineWidth = (int) mPaint.measureText(highlightText);
+        long start = highLrcRow.getStartTime();
+        long end = highLrcRow.getEndTime();
+
+        // highlight句子長度
+        int highWidth = (int) ((currentMillis - start) * 2.0f / (end - start) * highLineWidth);
+        if (highWidth > 0) {
+            mPaint.setColor(mHighLightRowColor);
+            mPaint.setTextSize(mHighLightFontSize);
+            Bitmap textBitmap = Bitmap.createBitmap(highWidth, highlightRowY + mPaddingY, Bitmap.Config.ARGB_8888);
+            Canvas textCanvas = new Canvas(textBitmap);
+            textCanvas.drawText(highlightText, 20, highlightRowY, mPaint);
+            canvas.drawBitmap(textBitmap, 0, 0, mPaint);
+        }
+    }
+
+
+
+    public void seekLrc(int position) {
+        if (mLrcRows == null || position < 0 || position > mLrcRows.size()) {
+            return;
+        }
+        mHighLightRow = position;
+        invalidate();
+
+    }
+
+
+    public void setLrc(List<LyricRow> lrcRows) {
+        mLrcRows = lrcRows;
+        invalidate();
+    }
+
+    /**
+     * 滾動歌詞
+     */
+    public void seekLrcToTime(long time) {
+
+        currentMillis = time;
+
+        for (int i = 0; i < mLrcRows.size(); i++) {
+            LyricRow current = mLrcRows.get(i);
+            LyricRow next = i + 1 == mLrcRows.size() ? null : mLrcRows.get(i + 1);
+
+            if ((time >= current.startTime && next != null && time < next.startTime)
+                    || (time > current.startTime && next == null)) {
+                seekLrc(i);
+                return;
+            }
+        }
+    }
+}
